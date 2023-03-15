88ff6b22699410ec0024ad6ed573e5c11878396a
Jessica Willan <j-willan@hotmail.co.uk>
2023-03-15 12:35:44 +0000

    this commit will add minimal styling to the CommentCard component, adding a background colour, border and resizing avatar

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,7 +1,7 @@
 const CommentCard = ({comment, avatar}) => {
   return (
     <div className="commentCard">
-      <img src={avatar} alt="Comment author avatar" />
+      <img src={avatar} alt="Comment author avatar" className='commentAvatar' />
       <p>{comment.author}</p>
       <p>{comment.body}</p>
     </div>
